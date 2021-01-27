59a740c2d2438ec739b5065fb4e8d0db06542a61
Ryan <ryan.fee625@gmail.com>
2021-01-27 07:51:01 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "scalr" {
+  ami                    = "ami-2757f631"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-0ebb1058ad727cfdb"
+  vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
+  key_name               = "ryan"
+}
