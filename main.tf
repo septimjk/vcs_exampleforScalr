12730376fbc9b53fca226c3723f40abf66a16b66
rtfee <ryan.fee625@gmail.com>
2021-07-13 14:11:59 -0400

    Updated instance type

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "scalr" {
   ami                    = "ami-2757f631"
-  instance_type          = "t2.nano"
+  instance_type          = "t2.micro"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
   key_name               = "ryan"
