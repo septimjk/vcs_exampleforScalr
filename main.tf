19041107c6278d4f4576bdd8707750adc247bdff
Ryan <ryan.fee625@gmail.com>
2021-09-23 13:11:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "scalr" {
