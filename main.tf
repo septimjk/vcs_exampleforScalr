f028d65a6fd7a1f632ef3e6210713bb2c2b402e5
Septi Mutiara Janing Kushardyansari <101852655+septimjk@users.noreply.github.com>
2022-05-23 14:22:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "scalr" {
   ami                    = "ami-0dc5e9ff792ec08e3"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0606418852dd6989a"
-  vpc_security_group_ids = "vpc-03c2e422e3ac04126"
+  vpc_security_group_ids = ["vpc-03c2e422e3ac04126"]
   key_name               = "keyDemo"
 }
